da29a812993870ca441d95a4d11f1f9de929351b
Vanessa Mota <mottavns@gmail.com>
2021-10-28 22:19:38 -0300

    are Balanço Ok

--- src/components/ResumeItem/index.tsx
+++ src/components/ResumeItem/index.tsx
@@ -3,12 +3,13 @@ import * as S from './styles';
 type Props = {
 	title: string;
 	value: number;
+	color?: string;
 }
-export const ResumeItem = ({ title, value }: Props) => {
+export const ResumeItem = ({ title, value, color }: Props) => {
 	return (
 		<S.Container>
 			<S.Title>{title}</S.Title>
-			<S.Info>R$ {value}</S.Info>
+			<S.Info color={color}>R$ {value}</S.Info>
 		</S.Container>
 	);
 
